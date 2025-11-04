ef5b029fa07294b8ca596269ad0e6ec5e5ff4b72
korolyam <70631951+korolyam@users.noreply.github.com>
2025-11-04 12:47:31 +0200

    Add files via upload

---
+++ 4.2/4.2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include "conio.h"
+#include "stdio.h"
+int main()
+{
+    double trash;
+    double a;
+    double n = 1;
+    std::cout << "Vvedite border a\n";
+    std::cin >> a;
+    if (a <= 1 || a >= 1.5)
+    {
+        std::cout << "Given progression will never be larger than a\n";
+        _getch();
+        return 0;
+    }
+    __asm
+    {
+        do_this:
+        fld1
+        fld n
+        fadd st(0), st(1)
+        fst n
+        fdivr st(0), st(1)
+        fadd st(0), st(1)
+        fcom a
+        fstsw ax
+        sahf
+        fstp trash
+        fstp trash
+        jae do_this
+    }
+    std::cout << "n = " << n << "\n";
+    _getch();
+    return 0;
+}
---
+++ 4.3/4.3.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include "conio.h"
+#include "stdio.h"
+int main()
+{
+    double trash;
+    double a = 0;
+    double b = 1;
+    int n = 11;
+    double sum = 0;
+    double help = -1;
+    double temp;
+    __asm
+    {
+        mov ecx, n
+        loopa:
+        fld1
+        fld a
+        fld b
+        fadd st(0), st(2)
+        fadd st(1), st(2)
+        fstp b
+        fst a
+        fld b
+        fdivr st(0), st(1)
+        fstp temp
+        mov n, ecx
+        and ecx, 1
+        cmp ecx, 0
+        je shmeven
+        fld temp
+        fld help
+        fmul st(1), st(0)
+        fstp help
+        fstp temp
+        shmeven:
+        mov ecx, n
+        fld temp
+        fld sum
+        fadd st(0), st(1)
+        fstp sum
+        fstp trash
+        fstp trash
+        fstp trash
+        loop loopa
+    }
+    std::cout << "Sum = " << sum << "\n";
+    _getch();
+    return 0;
+}
